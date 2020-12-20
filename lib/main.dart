1e8ff482d26c276059c9ee0917bb17fbd859e5ed
Vleis Walker <vleiswal@gmail.com>
2020-12-20 10:11:50 +0200

    container

--- lib/main.dart
+++ lib/main.dart
@@ -23,11 +23,47 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        heightFactor: 4,
-        widthFactor: 2,
-        child: Text(
-          'Flutter Essenstials',
-          style: TextStyle(fontSize: 20),
+        //heightFactor: 4,
+        //widthFactor: 2,
+        child: Container(
+          //color: Colors.blue,
+          padding: EdgeInsets.all(8),
+          //constraints: BoxConstraints.expand(),
+          alignment: Alignment.center,
+          width: 200,
+          height: 200,
+          //transform: Matrix4.rotationZ(0.5),
+          decoration: BoxDecoration(
+            color: Colors.blue,
+            border: Border.all(
+              width: 3,
+              color: Colors.purple,
+              style: BorderStyle.solid,
+            ),
+            //borderRadius: BorderRadius.circular(12),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey,
+                blurRadius: 4.0,
+                spreadRadius: 2.0,
+                //offset: Offset.
+              ),
+            ],
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.centerRight,
+              colors: [
+                Colors.blue,
+                Colors.green,
+                Colors.yellow,
+              ],
+            ),
+            shape: BoxShape.circle,
+          ),
+          child: Text(
+            'Flutter Essenstials',
+            style: TextStyle(fontSize: 20, color: Colors.white),
+          ),
         ),
       ),
     );
