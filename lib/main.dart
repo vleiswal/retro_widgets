e69703db8de0c1492b8c62e1f1baee1b358d0362
Vleis Walker <vleiswal@gmail.com>
2020-12-20 19:10:41 +0200

    rows_columns

--- lib/main.dart
+++ lib/main.dart
@@ -23,49 +23,81 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        //heightFactor: 4,
-        //widthFactor: 2,
-        child: Container(
-          //color: Colors.blue,
-          padding: EdgeInsets.all(8),
-          //constraints: BoxConstraints.expand(),
-          alignment: Alignment.center,
-          width: 200,
-          height: 200,
-          //transform: Matrix4.rotationZ(0.5),
-          decoration: BoxDecoration(
-            color: Colors.blue,
-            border: Border.all(
-              width: 3,
-              color: Colors.purple,
-              style: BorderStyle.solid,
+          child: Container(
+        height: 200,
+        color: Colors.red,
+        child: Row(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            ElevatedButton(
+              child: Text('Button 1'),
+              onPressed: () {},
             ),
-            //borderRadius: BorderRadius.circular(12),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.grey,
-                blurRadius: 4.0,
-                spreadRadius: 2.0,
-                //offset: Offset.
-              ),
-            ],
-            gradient: LinearGradient(
-              begin: Alignment.topLeft,
-              end: Alignment.centerRight,
-              colors: [
-                Colors.blue,
-                Colors.green,
-                Colors.yellow,
-              ],
+            ElevatedButton(
+              child: Text('Button 1'),
+              onPressed: () {},
             ),
-            shape: BoxShape.circle,
-          ),
-          child: Text(
-            'Flutter Essenstials',
-            style: TextStyle(fontSize: 20, color: Colors.white),
-          ),
+            ElevatedButton(
+              child: Text('Button 1'),
+              onPressed: () {},
+            ),
+          ],
         ),
-      ),
+      )),
     );
   }
 }
+
+
+// class MyHomePage extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: Center(
+//         //heightFactor: 4,
+//         //widthFactor: 2,
+//         child: Container(
+//           //color: Colors.blue,
+//           padding: EdgeInsets.all(8),
+//           //constraints: BoxConstraints.expand(),
+//           alignment: Alignment.center,
+//           width: 200,
+//           height: 200,
+//           //transform: Matrix4.rotationZ(0.5),
+//           decoration: BoxDecoration(
+//             color: Colors.blue,
+//             border: Border.all(
+//               width: 3,
+//               color: Colors.purple,
+//               style: BorderStyle.solid,
+//             ),
+//             //borderRadius: BorderRadius.circular(12),
+//             boxShadow: [
+//               BoxShadow(
+//                 color: Colors.grey,
+//                 blurRadius: 4.0,
+//                 spreadRadius: 2.0,
+//                 //offset: Offset.
+//               ),
+//             ],
+//             gradient: LinearGradient(
+//               begin: Alignment.topLeft,
+//               end: Alignment.centerRight,
+//               colors: [
+//                 Colors.blue,
+//                 Colors.green,
+//                 Colors.yellow,
+//               ],
+//             ),
+//             shape: BoxShape.circle,
+//           ),
+//           child: Text(
+//             'Flutter Essenstials',
+//             style: TextStyle(fontSize: 20, color: Colors.white),
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
